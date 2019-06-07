1e57d8f4ad0293394aa963af868d8413b48c0316
musoline <yruashvilinika@yahoo.com>
2019-06-07 16:58:01 +0400

    single post

--- single.php
+++ single.php
@@ -1,21 +1,19 @@
 <?php get_header(); ?>
 
-
- 
-<article class="single-article">
-  <div class="single-thumbnail">
-    <img src="<?php if(has_post_thumbnail()){ the_post_thumbnail();} ?>" alt="">
-  </div>
-</article>
-
-
-
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<?php the_title(); ?>
- <?php         if(has_post_thumbnail()){
-          the_post_thumbnail();  
-        } ?>
- <?php the_content(); ?>
+  <?php the_title(); ?>
+  <article class="single-article">
+    <div class="single-article-thumbnail">
+        <img src="<?php if(has_post_thumbnail()){ the_post_thumbnail();} ?>" alt="">
+    </div>
+    <div class="single-article-info">
+      <p class="single-article-text">
+        <?php the_content(); ?>      
+      </p>
+    </div>
+  </article>
+
+
  <?php echo get_the_date(); ?>
 
 <?php endwhile; ?>
